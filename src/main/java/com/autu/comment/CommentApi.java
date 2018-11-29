6da943478536aa1d2c46aa73653d7eca48ba6b04
wenhaofan <476382755@qq.com>
2018-11-29 20:00:20 +0800

    防止评论XSS注入

--- src/main/java/com/autu/comment/CommentApi.java
+++ src/main/java/com/autu/comment/CommentApi.java
@@ -5,6 +5,7 @@ import com.autu.article.ArticleService;
 import com.autu.common.controller.BaseController;
 import com.autu.common.model.entity.AgentUser;
 import com.autu.common.model.entity.Comment;
+import com.autu.common.safe.JsoupFilter;
 import com.jfinal.aop.Inject;
 import com.jfinal.kit.Ret;
 
@@ -18,6 +19,12 @@ public class CommentApi extends BaseController {
 	
 	public void save() {
 		Comment comment=getModel(Comment.class,"",true);
+		String name=JsoupFilter.getText(comment.getName());
+		String content=JsoupFilter.getText(comment.getContent());
+		
+		comment.setName(name);
+		comment.setContent(content);
+		
 		AgentUser agentUser=getAgentUser();
 		service.save(comment,agentUser!=null?agentUser.getCookie():"empty");
 		renderJson(Ret.ok());
