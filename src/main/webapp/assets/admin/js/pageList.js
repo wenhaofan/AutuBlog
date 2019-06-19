7268322fba3ee06ac23e4f04c2f1389169359a24
wenhaofan <476382755@qq.com>
2019-06-19 22:23:13 +0800

    fix

--- src/main/webapp/assets/admin/js/pageList.js
+++ src/main/webapp/assets/admin/js/pageList.js
@@ -38,18 +38,18 @@ layui.define([ 'table','fl','form','layer'], function(exports) {
 			},renderTable:function(data){
 				const table=layui.table;
 				layui.fl.renderTable({
-					elem:'#routeTable',
+					elem:'#pageTable',
 					 page: { count: 80, limit: 10 }
 	                , url: '/admin/api/page/list',
 	                where:data
 	                , cols: [[ //表头
 	                    { field: 'id', sort: true, title: "ID", width: 90 }
-	                    , { field: 'name', minWidth: 150, title: "标题" }
+	                  
 	                    , { templet:function(d){
 	                    	return d.article.pv;
 	                    }, width: 90, sort: true, title: "阅读量" }
 	                    , { field: 'pattern', title: "访问路径" }
-	                    , { field: 'remark', title: "备注" }
+	                  
 	                    , {   width: 90, templet: function(d){
 	                    	return d.isDeleted?'已删除':'正常';
 	                    }, title: "状态" }
@@ -58,13 +58,13 @@ layui.define([ 'table','fl','form','layer'], function(exports) {
 	                    	let tpl= '<button type="button" class="layui-btn layui-btn-xs page-edit-template"  ><a href="/admin/page/edit/'+d.id+'" pjax >修改模板</a></button>';
 	                    	
 	                    	if(d.articleId){
-	                    		tpl+='<button type="button" class="layui-btn layui-btn-xs page-edit"  ><a href="/admin/article/edit/'+d.articleId+'" pjax >修改文章</button>';;	               
+	                    		tpl+='<button type="button" class="layui-btn layui-btn-xs page-edit"  ><a href="/admin/article/edit/'+d.articleId+'" pjax >修改文章</a></button>';;	               
 	                    	}
 	                    
 	                    	if(d.isDeleted){
-	                    		tpl+=	' <button type="bbutton" class="layui-btn layui-btn-xs page-recover" data=id="'+d.id+'">恢复</button>';
+	                    		tpl+=	' <button type="bbutton" class="layui-btn layui-btn-xs page-recover" data-id="'+d.id+'">恢复</button>';
 	                    	}else{
-	                    		tpl+=	' <button type="bbutton" class="layui-btn layui-btn-xs page-remove" data=id="'+d.id+'">删除</button>';
+	                    		tpl+=	' <button type="bbutton" class="layui-btn layui-btn-xs page-remove" data-id="'+d.id+'">删除</button>';
 	                    	}
 	                    	
 	                    	return tpl;
@@ -74,12 +74,17 @@ layui.define([ 'table','fl','form','layer'], function(exports) {
 			},pjaxLoad:function(){
 				this.load();
 			},removePage:function(id){
-				fl.ajax({
-					url:"/admin/api/page/remove/"+id,
-					success:function(data){
-						fl.alertOkAndReload({title:"删除成功"});
-					}
-				})
+				layui.fl.alertConfirm({
+	                title: "是否确认删除？", then: function () {
+	                	layui.fl.ajax({
+	    					url:"/admin/api/page/remove/"+id,
+	    					success:function(data){
+	    						fl.alertOkAndReload({title:"删除成功"});
+	    					}
+	    				})
+	                }
+	            })
+				
 			},recoverPage:function(id){
 				fl.ajax({
 					url:"/admin/api/page/recover/"+id,
